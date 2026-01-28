4e45c9d15cf20f1e1f1d90ae07660c533ca1b0f1
unrealaru-hub <unreal.aru@gmail.com>
2026-01-28 06:35:55 +0530

    practice  day 01

---
+++ demo.java
@@ -0,0 +1,79 @@
+// // public class demo {
+
+// //     public static void main(String[] args) {
+// //         int m = 12;
+// //         float f = 2.3f;
+// //         double d = 34.3d;
+// //         byte b = 5;
+// //         char c =  'd';
+// //         String s = "arun";
+// //         boolean isPass = true;
+// //         short ss = 45;
+// //         long n = 23443l;
+// //         System.out.println(n);
+// //         System.out.println(ss);
+// //         System.out.println(isPass);
+// //         System.out.println("name : "  + s);
+// //         System.out.println(c);
+// //         System.out.println(b);
+// //         System.out.println(d);
+// //         System.out.println(f);
+// //         System.out.println(m);
+// //     }
+
+// // }
+
+// public class demo {
+
+//     public static void main(String[] args) {
+//         char c = 'e';
+//         int v = c + 1;
+//         System.out.println(v);
+//     }
+// }
+
+
+// public class demo {
+
+//     public static void main(String[] args) {
+//         int a = 40;
+//         int b = 30;
+
+//         if(a >= b  || a==b){
+//             System.out.println("satisfy");
+//         }else{
+//             System.out.println("not satisfy");
+//         }
+//     }
+// }
+
+public class demo {
+
+    public static void main(String[] args) {
+        int a = 126;
+        byte b = (byte)a;   //type casting 
+
+        byte c = 12;
+        a=c;    //type conversion because byte varry only 127 to -128
+                //also if int value is 12 then not working becasue data type is differnt and use type casting here
+         
+      byte r = 10;
+      byte r2 = 30;
+    //   int result = r * r2;
+    //   System.out.println(result);
+        
+    //     System.out.println(b);
+
+//arithmatic operator
+        int m = 7;
+        int m2 = 5;
+        float result2 = m + m2;
+        // System.out.println(result2);
+        System.out.println(m++);  //post increment
+        System.out.println(m);
+
+        System.out.println(--m2);  //pre decrement
+        System.out.println(m2);
+
+    }
+}
